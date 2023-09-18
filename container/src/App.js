f9d2ee91128be33a2862d803adfd68f6a64c825c
meitung473 <a8870506@gmail.com>
2023-09-18 22:52:18 +0800

    Added heading 2 'Hello' text in container

--- container/src/App.js
+++ container/src/App.js
@@ -4,6 +4,7 @@ export default () => {
     return (
         <div>
             <h1>Hello world</h1>
+            <h2>Another Feature Hello!!</h2>
             <hr />
             <MarketingApp />
         </div>
